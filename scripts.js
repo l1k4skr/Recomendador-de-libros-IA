246964dc964723c90f7e17fcb9f286761ebbc573
l1k4skr <82717429+l1k4skr@users.noreply.github.com>
2024-02-19 16:32:11 -0300

    Refactor code to dynamically load and display books

--- scripts.js
+++ scripts.js
@@ -1,14 +1,45 @@
-const form = document.querySelector('form');
-const listaLibros = document.querySelector('.lista-libros');
-console.log(listaLibros)
+const form = document.querySelector("form");
+const listaLibros = document.querySelector(".lista-libros");
+let books = []
+
 let counter = 1;
 
-form.addEventListener('submit', function(event) {
-    listaLibros.innerHTML += `
-        <li>Libro ${counter}</li>
 
-    `;
-    counter++;
+const leerLibros = async () => {
+	try {
+		const response = await fetch("books.json");
+		const data = await response.json();
+		books = data;
+	} catch (error) {
+		console.error("Error: ", error);
+	}
+}
+
+const procesarLibros = async () => {
+	books.map((book) => { // Esto debe cambiar , no se puede usar map
+		console.log(book);
+		const { id, title: titulo, author: autor, year, genre: genero, price: precio, cover } = book;
+		listaLibros.innerHTML += `
+		<li>
+		<span>${id}</span>
+		<h3>${titulo}</h3>
+		<p>${autor}</p>
+		<p>${year}</p>
+		<p>${genero}</p>
+		<p>${precio}</p>
+		<img src="${cover}" alt="${titulo}">
+		
+		</li>
+	`;
+		counter++;
+
+	});
+}
+form.addEventListener("submit", async function (event) {
+	event.preventDefault();
+	if (books.length === 0) {
+		await leerLibros();
+		await procesarLibros();
+	}
+	
 });
-
-
